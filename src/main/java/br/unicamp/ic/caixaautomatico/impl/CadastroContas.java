42c73add19fdf1e55c0229a019fdc189e8fe7ab3
André Vieira <andre.vieira@outlook.com>
2016-03-15 17:53:04 -0300

    feito

--- src/main/java/br/unicamp/ic/caixaautomatico/impl/CadastroContas.java
+++ src/main/java/br/unicamp/ic/caixaautomatico/impl/CadastroContas.java
@@ -8,31 +8,34 @@ import br.unicamp.ic.caixaautomatico.spec.IConta;
 
 public class CadastroContas implements ICadastroContas {
 	List<ContaBase> registroDeContas = new ArrayList<ContaBase>();
-	
+
 	static int contador;
 
 	@Override
 	public IConta buscaConta(int numeroConta) {
-		// TODO Auto-generated method stub
+		if (this.registroDeContas.contains(numeroConta)) {
+			return registroDeContas.get(numeroConta);
+		}
+
 		return null;
 	}
 
 	@Override
 	public IConta criarContaCorrente(String titular, float saldoAtual, int senha) {
 		contador++;
-		
+
 		IConta conta = new ContaCor(titular, saldoAtual, contador, senha);
-		
+
 		return conta;
 	}
 
 	@Override
 	public IConta criarContaEspecial(String titular, float saldoAtual, int senha) {
 		contador++;
-		
+
 		IConta conta = new ContaEsp(titular, saldoAtual, contador, senha);
-		
+
 		return conta;
 	}
-	
+
 }
